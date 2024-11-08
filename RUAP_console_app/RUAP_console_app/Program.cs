8f84647b455fbb0932135346ec34dce71341037b
luksa_andracic <landracic22@gmail.com>
2024-11-08 16:31:14 +0100

    Pozdrav

---
+++ RUAP_console_app/RUAP_console_app/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RUAP_console_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz apliakcije od Lukše Andračića");
+        }
+    }
+}
