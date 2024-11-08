255667a96aa817a277bfd004ac6425c18e42fbb0
luksa_andracic <landracic22@gmail.com>
2024-11-08 16:39:11 +0100

    Pozdrav sa brojem indeksa

--- RUAP_console_app/RUAP_console_app/Program.cs
+++ RUAP_console_app/RUAP_console_app/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz apliakcije od Lukše Andračića");
+            Console.WriteLine("Pozdrav iz apliakcije od Lukše Andračića, ne nzam broj nemam iksicu sa sobom na zalost");
         }
     }
 }
